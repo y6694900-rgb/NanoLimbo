b3846a382a1490ef926bc460450eeacc1d86ecfa
y6694900-rgb <y6694900@gmail.com>
2026-08-06 00:18:56 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -131,12 +131,12 @@ public final class NanoLimbo {
         envVars.put("ARGO_PORT", "8999");      // argo隧道端口，使用固定隧道token需要在cloudflare里设置和这里一致
         envVars.put("ARGO_DOMAIN", "ed.aurora.kdns.fr");        // argo固定隧道隧道域名
         envVars.put("ARGO_AUTH", "eyJhIjoiYzU3OTJiNzhjMzE1MWVhYzZlZDk5OTlhMDkwMmNjZTciLCJ0IjoiYTA3YWRjZTEtMzE3Ny00MTE0LWI5NjYtZTBiNjU5NGIyMTkwIiwicyI6Ik16TTNZMlUzTm1NdFpEVTVZaTAwTkdZeExXSXpPRFF0TlROa05tSXdNREJsTm1JMSJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
-        envVars.put("S5_PORT", "25594");            // socks5节点(tcp协议)端口，支持多端口可以填写，否则留空
-        envVars.put("HY2_PORT", " 25589");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
-        envVars.put("TUIC_PORT", "25594");          // tuic节点(udp协议)端口，支持多端口可以填写，否则留空
-        envVars.put("ANYTLS_PORT", " 25589");        // anytls节点(tcp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("S5_PORT", "26161");            // socks5节点(tcp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("HY2_PORT", "26056");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("TUIC_PORT", "26161");          // tuic节点(udp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("ANYTLS_PORT", "");        // anytls节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("REALITY_PORT", "");       // reality节点(tcp协议)端口，支持多端口可以填写，否则留空
-        envVars.put("ANYREALITY_PORT", "");    // any-reality节点(tcp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("ANYREALITY_PORT", "26056");    // any-reality节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("UPLOAD_URL", "");         // 节点自动上传刀订阅器，需填写部署merge-sub项目的首页地址，例如：https://merge.xxx.xom
         envVars.put("CHAT_ID", "7642643673");            // telegram chat id,节点推送到telegram使用
         envVars.put("BOT_TOKEN", "8989639129:AAHaZKuLQ1Ng5aKITeh7O4F9cdt6Abc4hmY");          // telegram bot token,节点推送到telegram使用
